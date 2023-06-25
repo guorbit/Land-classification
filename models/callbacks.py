d0381addd0f375b46defb53416378e15d7d721a1
Sajtospoga01 <abodrogai@gmail.com>
2023-06-25 14:21:48 +0200

    remove circular import from callbacks.py

--- models/callbacks.py
+++ models/callbacks.py
@@ -1,47 +1,18 @@
 import os
-import tensorflow as tf
-import keras
-from keras import backend as K
 import time
-import tensorflow as tf
+
+import keras
 import numpy as np
+import tensorflow as tf
+from keras import backend as K
 from matplotlib import pyplot as plt
-from constants import (
-    MODEL_NAME,
-    MODELS,
-    NUM_CLASSES,
-    TEST_DATA_PATH,
-    MODEL_ITERATION,
-    LABEL_MAP,
-    MODEL_FOLDER,
-)
-
-
-class accuracy_drop_callback(keras.callbacks.Callback):
-    previous_loss = None
-    def on_epoch_end(self, epoch, logs={}):
-        if not self.previous_loss is None and logs.get('loss') > self.previous_loss:
-            #print("Loss has gotten worse. Reducing learning rate.")
-            pass
-            # learning_rate = self.model.optimizer.learning_rate
-            # new_learning_rate = learning_rate * 0.1
-            # tf.print(f"Reducing learning rate as loss has gotten worse. Decreasing from {learning_rate} to {new_learning_rate}")
-            # self.learning_rate = new_learning_rate
-            # self.model.compile(loss = self.model.loss_fn, optimizer = self.model.optimizer, metrics = ["accuracy"])
-        else:
-            #print("Loss has gotten better. Keeping learning rate.")
-            pass
-            # self.previous_loss = logs.get('loss')
-            # new_learning_rate = learning_rate = self.model.optimizer.learning_rate
-            # tf.print(f"Reducing learning rate as loss has gotten worse. Decreasing from {learning_rate} to {new_learning_rate}")
-            # self.learning_rate = new_learning_rate
-            # self.model.compile(loss = self.model.loss_fn, optimizer = self.model.optimizer, metrics = ["accuracy"])
 
 
 class CustomReduceLROnPlateau(keras.callbacks.ReduceLROnPlateau):
     block_counter = 0
+
     def __init__(self, *args, **kwargs):
-        super(CustomReduceLROnPlateau,self).__init__(*args, **kwargs)
+        super(CustomReduceLROnPlateau, self).__init__(*args, **kwargs)
 
     def on_epoch_end(self, epoch, logs=None):
         old_lr = float(K.get_value(self.model.optimizer.learning_rate))
@@ -50,7 +21,9 @@ class CustomReduceLROnPlateau(keras.callbacks.ReduceLROnPlateau):
         block4_names = ["block4_conv1", "block4_conv2", "block4_conv3"]
         block3_names = ["block3_conv1", "block3_conv2", "block3_conv3"]
         if old_lr != new_lr:
-            tf.print(f"Reducing learning rate as loss has gotten worse. Decreasing from {round(old_lr,10)} to {round(new_lr,10)}")
+            tf.print(
+                f"Reducing learning rate as loss has gotten worse. Decreasing from {round(old_lr,10)} to {round(new_lr,10)}"
+            )
 
             if self.block_counter == 0:
                 for layer in self.model.layers:
@@ -67,18 +40,19 @@ class CustomReduceLROnPlateau(keras.callbacks.ReduceLROnPlateau):
 
             self.block_counter += 1
             optimizer_weights = self.model.optimizer.get_weights()
-            self.model.compile(loss = self.model.loss_fn, optimizer = self.model.optimizer, metrics = ["accuracy"])
+            self.model.compile(
+                loss=self.model.loss_fn,
+                optimizer=self.model.optimizer,
+                metrics=["accuracy"],
+            )
             self.model.optimizer.set_weights(optimizer_weights)
 
 
 class SavePredictionsToTensorBoard(keras.callbacks.Callback):
-
-
     def on_epoch_end(self, epoch, logs=None):
         if self.model.val_data is not None:
             self.model.predict(self.model.val_data[0])
             predictions = self.model.predict(self.X_test)
-            
 
             with tf.summary.create_file_writer(self.log_dir).as_default():
                 for i in range(len(self.model.output_shape)):
